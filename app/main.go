f0f2ce5387e0cdbf9156e06671d3f0f014382c7c
logan1o1 <saswatm2706@gmail.com>
2025-07-10 03:20:06 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
@@ -10,20 +12,59 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
+type responseHeader struct {
+	correlationId int32
+}
+
+type responseMessage struct {
+	messageSize int32
+	header      responseHeader
+}
+
+func (r *responseMessage) Byte() []byte {
+	buff := bytes.Buffer{}
+	binary.Write(&buff, binary.BigEndian, r.messageSize)
+	binary.Write(&buff, binary.BigEndian, r.header.correlationId)
+
+	return buff.Bytes()
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+
+	buff := make([]byte, 1024)
+	_, err := conn.Read(buff)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		return
+	}
+
+	response := responseMessage{
+		header: responseHeader{
+			correlationId: 7,
+		},
+	}
+
+	_, err = conn.Write(response.Byte())
+	if err != nil {
+		fmt.Println("Error writing into conn: ", err.Error())
+	}
+}
+
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConn(conn)
 	}
 }
